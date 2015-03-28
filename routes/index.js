c3e5f8e71f8dcf1ae14a4da27a00a3825245d9b2
Evan Dorsky <evan.dorsky@icloud.com>
2015-03-28 18:58:32 -0400

    Song list works

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var song = require('./song.js');
 module.exports = {
 	home: function(req, res) {
 		song.getSongs(function(songs) {
-			console.log('songs');
-			console.log(songs);
-			res.render('home', songs);
+			res.render('home', { songs: songs });
 		});
 	}
 }
