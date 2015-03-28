161740cc4b5ae1536019dfc4085cebdd3d948a37
Evan Dorsky <evan.dorsky@icloud.com>
2015-03-28 17:02:02 -0400

    Initial webserver

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+
+// routes
+var index = require('./routes/index')
+
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.get('/', index.home);
+
+app.listen(process.env.PORT || 3000);
