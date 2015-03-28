351dc05d776c4a32f468a32f9bf76b9e36652e2e
Evan Dorsky <evan.dorsky@icloud.com>
2015-03-28 18:57:01 -0400

    Update partial

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var song = require('./song.js');
 module.exports = {
 	home: function(req, res) {
 		song.getSongs(function(songs) {
+			console.log('songs');
+			console.log(songs);
 			res.render('home', songs);
 		});
 	}
