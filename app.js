73655386f1d4e9ccb690edc4f0ffcb485108c665
Evan Dorsky <evan.dorsky@icloud.com>
2015-03-28 20:56:45 -0400

    Basic client->server sockets

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3001);
 
 var PORT = process.env.PORT || 3000;
 var MONGO = process.env.MONGOURI_WIKI || 'mongodb://localhost/test';
@@ -33,7 +37,23 @@ app.get('/', index.home);
 //song router
 app.use('/song', song);
 
+// sockets
+var songQueue = [];
+
+io.on('connection', function(socket) {
+  console.log('New connection');
+  socket.on('queue-add', function(data) {
+    songQueue.push(data.songId);
+    console.log('songQueue');
+    console.log(songQueue);
+  });
+
+  socket.on('queue-remove', function(data) {
+    var index = songQueue.indexOf(data.songId);
+    songQueue.splice(index, 1);
+  });
+})
 
 app.listen(PORT, function() {
-  console.log("App running on port",PORT);
+  console.log('App running on port',PORT);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
-var $songs = $(".song");
+var socket = io.connect('http://localhost:3001');
+
+var $songs = $(".song-thumb");
 
 $songs.click(function(event) {
   var $song = $(this);
   
-})
+  socket.emit('queue-add', {
+    songId: $song.attr('id')
+  });
+});
