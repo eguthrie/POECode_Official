873ea6ee9f5739170d890f4c24045d8aba681fc8
Ben Kahle <ben@kahle.org>
2015-05-08 22:55:04 -0400

    began midi parsing.

--- GPIO_MIDI_Sandbox/MIDI_File_Parser.js
+++ GPIO_MIDI_Sandbox/MIDI_File_Parser.js
@@ -14,43 +14,69 @@ var midiFile =  MF(myBuffer);
 
 //calculating duration between ticks
 var ticksPerBeat = midiFile.tracks.ticksPerBeat;
-var uSecondsInMinute= 600000;
-var BPM= 120;
-var BPuS= BPM/uSecondsInMinute;
-var ticksPeruS= ticksPerbeat*BPuS;
-var uSperTick= 1/ticksPeruS;
+// var uSecondsInMinute= 600000;
+// var BPM= 120;
+// var BPuS= BPM/uSecondsInMinute;
+// var ticksPeruS= ticksPerbeat*BPuS;
+// var uSperTick= 1/ticksPeruS;
 
 //there are only so many note numbers we care about. 
 //All in a dictionary so we can change the state of that note
 
-var notes = {}
-for (var i = 52; i <= 81; i++) {
-	notes[i]=0x0;
-};
+// var notes = {}
+// for (var i = 52; i <= 81; i++) {
+// 	notes[i]=0x0;
+// };
 
-var noteNumberMapping = function (notes, state, midiTick) {
+// var noteNumberMapping = function (notes, state, midiTick) {
 	
-	notes[midiTick[noteNumber]]=state;
+// 	notes[midiTick[noteNumber]]=state;
 
+// }
+
+// var updateOutput = function(output, notes) {
+
+// 	output=
+// }
+var getTempo = function(mf) {
+	for (var i = 0; i < mf.tracks[0].length; i++) {
+		var metaTick = mf.tracks[0][i];
+		if (metaTick.subtype === "setTempo") {
+			return metaTick.microsecondsPerBeat;
+		}
+	};
 }
 
-var updateOutput = function(output, notes) {
-
-	output=
+var getMicro = function(diff) {
+	var hrtime = diff ? process.hrtime(diff) : process.hrtime();
+	return {full: hrtime, us: hrtime[0]*1000000+hrtime[1]/1000};//current time in microseconds
 }
-//generating output of the notes
-for (var i = 0; i <= midiFile.tracks[1].length; i++) {
-	var midiTick=midiFile.tracks[1][i];
-	var type =midiTick[type]
-	var subtype = midiTick[subtype]
 
-	if (type=="meta") {
+//generating output of the note
+var playSong = function(mf, tempo) {
+	var lastMicro = getMicro();
+	for (var i = 0; i <= midiFile.tracks[1].length - 1; i++) {
+		var midiTick = midiFile.tracks[1][i];
+		var deltaTimeTicks = midiTick.deltaTime;
+		var waitMicro = (tempo/ticksPerBeat)*deltaTimeTicks;
+		var type = midiTick.type;
+		var subtype = midiTick.subtype;
+		var noteNumber = midiTick.noteNumber;
+		var deltaMicro = getMicro(lastMicro.full);
+		while (deltaMicro.us < waitMicro) {
+			//busy wait?
+		}
 
-	}
-	if (type=="channel" && (subtype=='noteOn' || subtype=='noteOff'))}
-		state= subtype=='noteOn' ? 1:0
-		noteNumberMapping(notes, state, midiTick)
-		updateOutput(output, notes)
+		if (type === "meta") {
 
-	}
-};
+		}
+		if (type === "channel" && (subtype === 'noteOn' || subtype === 'noteOff')){
+			state = subtype === 'noteOn' ? 1:0;
+			// noteNumberMapping(notes, state, midiTick);
+			// updateOutput(output, notes);
+			console.log(subtype, noteNumber);
+		}
+	};
+}
+var tempo = getTempo(midiFile)
+playSong(midiFile, tempo);
